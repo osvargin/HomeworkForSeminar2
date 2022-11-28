bb8c3f1748e0083c1b322a700fa9dcc002b9f120
osvargin <oleg_197@bk.ru>
2022-11-28 16:05:27 +0300

    add Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+
+Console.Clear();
+
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int num1 = num / 100 * 100;
+int num2 = num % 10;
+int res = (num - num1 - num2) / 10;
+
+Console.WriteLine($"Вторая цифра числа {num} - {res}");
